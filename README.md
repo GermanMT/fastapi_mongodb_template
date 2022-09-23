481f865aedef91e00762402768f82c5a53c98b5f
GermanMT <germanoctako@gmail.com>
2022-09-23 16:25:50 +0200

    chore: Improve readme md

--- README.md
+++ README.md
@@ -7,3 +7,19 @@
 2. Run command 'docker-compose up --build'
 
 3. Enter [here](http://0.0.0.0:8000/docs)
+
+## Testing
+
+1. Create a virtual enviroment with command 'python3 -m venv tutorial-env' and python 3.10
+
+2. Execute the enviroment with command 'source tutorial-env/bin/activate'
+
+3. Install production requirements with command 'pip install -r requirements.txt'
+
+4. Install develop requirements with command 'pip install -r requirements_dev.txt'
+
+5. Run tests with command 'pytest test/'
+
+## Analyse code
+
+You can analyse your code best practices after install develop requirements with commands 'mypy app' and 'prospector' in the root folder of the project.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name = 'fastapi_mongodb_template',
-    version = '0.0.6',
+    version = '1.1.0',
     author = 'Antonio Germán Márquez Trujillo',
     author_email = 'amtrujillo@us.es',
     description = 'Template using FastAPI and MongoDB',
@@ -14,20 +14,19 @@ setup(
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/GermanMT/fastapi_mongodb_template',
     classifiers = [
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10'
     ],
     python_requires = '>=3.10',
     install_requires = [
         'wheel==0.37.1',
         'uvicorn==0.18.2',
         'fastapi==0.79.0',
+        'email-validator==1.3.0',
         'pymongo==4.2.0',
         'motor==3.0.0',
         'motor-stubs==1.7.1',
         'python-dotenv==0.20.0',
-        'requests==2.28.1'
+        'requests==2.28.1',
         'types-requests==2.28.8',
         'types-setuptools==63.4.0'
     ],
