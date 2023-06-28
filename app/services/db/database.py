ab9a40a7739fa08ee4e4ec65ac34e644710c70ec
GermanMT <germanoctako@gmail.com>
2023-06-28 16:55:55 +0200

    chore: Add mongo motor info to readme

--- app/services/db/database.py
+++ app/services/db/database.py
@@ -10,6 +10,6 @@ def get_collection(collection_name: str) -> AsyncIOMotorCollection:
     client = AsyncIOMotorClient(settings.DB_URI)
     match collection_name:
         case 'students':
-            return client.depex.get_collection('students')
+            return client.students.get_collection('students')
         case _:
             raise Exception('Is not a valid collection of the database!')
