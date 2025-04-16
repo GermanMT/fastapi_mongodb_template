4d3af448be89444d99ed06cc292b9c38b51e64a1
GermanMT <germanoctako@gmail.com>
2025-04-17 00:46:15 +0200

    feat: Improved tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,24 +1,27 @@
 from typing import Any
 
-import pytest
+import pytest_asyncio
 from bson import ObjectId
 from mongomock_motor import AsyncMongoMockClient
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture()
 async def mongo_mock(monkeypatch: Any) -> None:
     client = AsyncMongoMockClient()
-    database = client.get_database('TestStudentDB')
-    collection = database.get_collection('students')
+    database = client.get_database("TestStudentDB")
+    collection = database.get_collection("students")
 
     student_data: dict[str, Any] = {
-        '_id': ObjectId('6329cd902186c0e6c5fa5eef'),
-        'name': 'Myke',
-        'surname': 'Fernandez',
-        'age': 38,
-        'phone': '678 340 253'
+        "_id": ObjectId("6329cd902186c0e6c5fa5eef"),
+        "name": "Myke",
+        "surname": "Fernandez",
+        "age": 38,
+        "phone": "678 340 253"
     }
 
     await collection.insert_one(student_data)
 
-    monkeypatch.setattr('app.services.student_service.student_collection', collection)
+    monkeypatch.setattr(
+        "app.services.student_service.get_collection",
+        lambda name: collection
+    )
--- test/controllers/test_student_controller.py
+++ test/controllers/test_student_controller.py
@@ -11,192 +11,175 @@ client = TestClient(app)
 
 @pytest.mark.asyncio
 async def test_create_student_ok(mongo_mock: Any) -> None:
-    await mongo_mock
-
     student_data = {
-        'name': 'John',
-        'surname': 'Doe',
-        'age': 24,
-        'phone': '619 527 721'
+        "name": "John",
+        "surname": "Doe",
+        "age": 24,
+        "phone": "619 527 721"
     }
 
     response = client.post(
-        '/student',
+        "/student",
         json=student_data
     )
 
     assert response.status_code == 201
     inserted_student = response.json()
-    assert inserted_student['name'] == student_data['name']
-    assert inserted_student['surname'] == student_data['surname']
-    assert inserted_student['age'] == student_data['age']
-    assert inserted_student['phone'] == student_data['phone']
+    assert inserted_student["name"] == student_data["name"]
+    assert inserted_student["surname"] == student_data["surname"]
+    assert inserted_student["age"] == student_data["age"]
+    assert inserted_student["phone"] == student_data["phone"]
 
 
 @pytest.mark.asyncio
 async def test_create_student_empty_name_surname(mongo_mock: Any) -> None:
-    await mongo_mock
-
     student_data = {
-        'name': '',
-        'surname': '',
-        'age': 24,
-        'phone': '619 527 721'
+        "name": "",
+        "surname": "",
+        "age": 24,
+        "phone": "619 527 721"
     }
 
     response = client.post(
-        '/student',
+        "/student",
         json=student_data
     )
 
     assert response.status_code == 422
     error = response.json()
-    assert error['message'][0] == 'name: ensure this value has at least 1 characters'
-    assert error['message'][1] == 'surname: ensure this value has at least 1 characters'
+    assert error["detail"][0]["loc"][1] == "name"
+    assert error["detail"][0]["msg"] == "String should have at least 1 character"
+    assert error["detail"][1]["loc"][1] == "surname"
+    assert error["detail"][1]["msg"] == "String should have at least 1 character"
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('age', (1, 50, 100))
+@pytest.mark.parametrize("age", (1, 50, 100))
 async def test_create_student_ok_age(mongo_mock: Any, age: int) -> None:
-    await mongo_mock
-
     student_data = {
-        'name': 'John',
-        'surname': 'Doe',
-        'age': age,
-        'phone': '619 527 721'
+        "name": "John",
+        "surname": "Doe",
+        "age": age,
+        "phone": "619 527 721"
     }
 
     response = client.post(
-        '/student',
+        "/student",
         json=student_data
     )
 
     assert response.status_code == 201
     inserted_student = response.json()
-    assert inserted_student['name'] == student_data['name']
-    assert inserted_student['surname'] == student_data['surname']
-    assert inserted_student['age'] == student_data['age']
-    assert inserted_student['phone'] == student_data['phone']
+    assert inserted_student["name"] == student_data["name"]
+    assert inserted_student["surname"] == student_data["surname"]
+    assert inserted_student["age"] == student_data["age"]
+    assert inserted_student["phone"] == student_data["phone"]
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('age', (-50, -1, 0))
+@pytest.mark.parametrize("age", (-50, -1, 0))
 async def test_create_student_bad_age(mongo_mock: Any, age: int) -> None:
-    await mongo_mock
-
     student_data = {
-        'name': 'John',
-        'surname': 'Doe',
-        'age': age,
-        'phone': '619 527 721'
+        "name": "John",
+        "surname": "Doe",
+        "age": age,
+        "phone": "619 527 721"
     }
 
     response = client.post(
-        '/student',
+        "/student",
         json=student_data
     )
 
     assert response.status_code == 422
     error = response.json()
-    assert error['message'][0] == 'age: ensure this value is greater than 0'
+    print(error)
+    assert error["detail"][0]["loc"][1] == "age"
+    assert error["detail"][0]["msg"] == "Input should be greater than 0"
 
 
 @pytest.mark.asyncio
 async def test_create_student_bad_phone(mongo_mock: Any) -> None:
-    await mongo_mock
-
     student_data = {
-        'name': 'John',
-        'surname': 'Doe',
-        'age': 24,
-        'phone': '619 asd 721'
+        "name": "John",
+        "surname": "Doe",
+        "age": 24,
+        "phone": "619 asd 721"
     }
 
     response = client.post(
-        '/student',
+        "/student",
         json=student_data
     )
 
     assert response.status_code == 422
     error = response.json()
-    assert error['message'][0] == 'phone: The phone 619 asd 721 is not correct'
+    assert error["detail"][0]["loc"][1] == "phone"
+    assert error["detail"][0]["msg"] == "Value error, The phone 619 asd 721 is not correct"
 
 
 @pytest.mark.asyncio
 async def test_read_student(mongo_mock: Any) -> None:
-    await mongo_mock
-
-    response = client.get('/student/6329cd902186c0e6c5fa5eef')
+    response = client.get("/student/6329cd902186c0e6c5fa5eef")
     assert response.status_code == 200
     readed_student = response.json()
-    assert readed_student['name'] == 'Myke'
-    assert readed_student['surname'] == 'Fernandez'
-    assert readed_student['age'] == 38
-    assert readed_student['phone'] == '678 340 253'
+    assert readed_student["name"] == "Myke"
+    assert readed_student["surname"] == "Fernandez"
+    assert readed_student["age"] == 38
+    assert readed_student["phone"] == "678 340 253"
 
 
 @pytest.mark.asyncio
 async def test_read_student_not_found(mongo_mock: Any) -> None:
-    await mongo_mock
-
-    response = client.get('/student/632c2636db39c267a803f1ed')
+    response = client.get("/student/632c2636db39c267a803f1ed")
     assert response.status_code == 404
     error = response.json()
-    assert error['message'][0] == 'Student with id 632c2636db39c267a803f1ed not found'
+    assert error["detail"][0] == "Student with id 632c2636db39c267a803f1ed not found"
 
 
 @pytest.mark.asyncio
 async def test_update_student(mongo_mock: Any) -> None:
-    await mongo_mock
-
     updated_student_data = {
         "name": "Myke",
         "surname": "Fernandez",
         "age": 20,
         "phone": "678 340 253"
     }
-    response = client.put('/student/6329cd902186c0e6c5fa5eef', content=dumps(updated_student_data))
+    response = client.put("/student/6329cd902186c0e6c5fa5eef", content=dumps(updated_student_data))
     assert response.status_code == 200
     json_response = response.json()
     assert (
-        json_response['message'] == 'Student with id 6329cd902186c0e6c5fa5eef updated successfully'
+        json_response["message"] == "Student with id 6329cd902186c0e6c5fa5eef updated successfully"
     )
 
 
 @pytest.mark.asyncio
 async def test_update_student_not_found(mongo_mock: Any) -> None:
-    await mongo_mock
-
     updated_student_data = {
         "name": "Myke",
         "surname": "Fernandez",
         "age": 20,
         "phone": "678 340 253"
     }
-    response = client.put('/student/632c2636db39c267a803f1ed', content=dumps(updated_student_data))
+    response = client.put("/student/632c2636db39c267a803f1ed", content=dumps(updated_student_data))
     assert response.status_code == 404
     error = response.json()
-    assert error['message'][0] == 'Student with id 632c2636db39c267a803f1ed not found'
+    assert error["detail"][0] == "Student with id 632c2636db39c267a803f1ed not found"
 
 
 @pytest.mark.asyncio
 async def test_delete_employee(mongo_mock: Any) -> None:
-    await mongo_mock
-
-    response = client.delete('/student/6329cd902186c0e6c5fa5eef')
+    response = client.delete("/student/6329cd902186c0e6c5fa5eef")
     assert response.status_code == 200
     json_response = response.json()
     assert (
-        json_response['message'] == 'Student with id 6329cd902186c0e6c5fa5eef deleted successfully'
+        json_response["message"] == "Student with id 6329cd902186c0e6c5fa5eef deleted successfully"
     )
 
 
 @pytest.mark.asyncio
 async def test_delete_employee_not_found(mongo_mock: Any) -> None:
-    await mongo_mock
-
-    response = client.delete('/student/632c2636db39c267a803f1ed')
+    response = client.delete("/student/632c2636db39c267a803f1ed")
     assert response.status_code == 404
     error = response.json()
-    assert error['message'][0] == 'Student with id 632c2636db39c267a803f1ed not found'
+    assert error["detail"][0] == "Student with id 632c2636db39c267a803f1ed not found"
